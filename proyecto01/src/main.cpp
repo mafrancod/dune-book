869b971f8885bae271e7051f092e8b7b5c2bcfa4
Maria Jose Franco <mafrancod@unal.edu.co>
2024-05-22 10:38:12 -0500

    Cambiando estado de proyecto01 con cmake

---
+++ proyecto01/src/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+
+int main(){
+	std::cout<<"Hola mundo!"<<std::endl;
+	std::cout<<"Let's go"<<std::endl;
+    std::cout<<"Raíz cuadrada de 2 es =" <<sqrt(2.0)<<std::endl;
+
+	return 0;
+}
